3fd4c74df0085cd8edd11ae0c7ed6cd6616f4ead
Connor Nelson <Connor@ConnorNelson.com>
2024-12-19 18:21:09 -0700

    Rewrite in rust (#5)
    * Rewrite in rust
    
    * Fix
    
    * Fix

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,17 @@
 # syntax=docker/dockerfile:1
 
-FROM alpine:latest AS builder
-
-RUN apk add --no-cache gcc musl-dev
-
-COPY exec-suid.c .
-
-ENV CC="gcc -static -O3 -s"
-RUN $CC -o /usr/bin/exec-suid exec-suid.c
+FROM rust:latest AS builder
+RUN apt-get update && apt-get install -y musl-tools && rm -rf /var/lib/apt/lists/*
+RUN rustup target add x86_64-unknown-linux-musl
+WORKDIR /usr/src/exec-suid
+COPY Cargo.toml ./
+COPY src ./src
+RUN find . && cargo build --release --target x86_64-unknown-linux-musl
 
 FROM python:latest
-
 RUN pip install pytest pyyaml
-
 RUN useradd -m hacker
-
-COPY --from=builder --chown=0:0 --chmod=6755 /usr/bin/exec-suid /usr/bin/exec-suid
-
+COPY --from=builder --chmod=6755 /usr/src/exec-suid/target/x86_64-unknown-linux-musl/release/exec-suid /usr/bin/exec-suid
 COPY tests /tests
 
 CMD ["pytest", "-v", "/tests"]
---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use nix::sys::stat::{self, Mode};
+use nix::unistd::{self, Uid, Gid};
+use std::{env, process};
+use std::ffi::CString;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::{Path, PathBuf};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        eprintln!("Usage: {} [path]", args[0]);
+        process::exit(1);
+    }
+
+    let path = Path::new(&args[if args.len() == 2 { 1 } else { 2 }]);
+    if let Err(err) = validate_secure_path(path) {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    }
+    let (exec_argv, mut script_argv) = parse_header(path).unwrap_or_else(|err| {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    });
+    if exec_argv[0] != args[0] {
+        eprintln!("{}: Executable path does not match", path.display());
+        process::exit(1);
+    }
+
+    script_argv.push(path.to_str().unwrap().to_string());
+
+    let stat = stat::stat(path).unwrap_or_else(|err| {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    });
+    let mode = stat::Mode::from_bits_truncate(stat.st_mode);
+    let new_euid = if mode.contains(Mode::S_ISUID) { Uid::from_raw(stat.st_uid) } else { Uid::current() };
+    let new_egid = if mode.contains(Mode::S_ISGID) { Gid::from_raw(stat.st_gid) } else { Gid::current() };
+    if let Err(err) = unistd::setegid(new_egid) {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    }
+    if let Err(err) = unistd::seteuid(new_euid) {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    }
+
+    unistd::execve(
+        &CString::new(script_argv[0].clone()).unwrap(),
+        &script_argv.iter().map(|arg| CString::new(arg.as_str()).unwrap()).collect::<Vec<_>>(),
+        &[] as &[CString],
+    ).unwrap_or_else(|err| {
+        eprintln!("{}: {}", path.display(), err);
+        process::exit(1);
+    });
+}
+
+fn parse_header(path: &Path) -> io::Result<(Vec<String>, Vec<String>)> {
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+    let first_line = reader.lines().next().unwrap()?;
+    if !first_line.starts_with("#!") {
+        return Err(io::Error::new(io::ErrorKind::Other, "Header does not start with #!"));
+    }
+    let tokens: Vec<String> = first_line[2..].split_whitespace().map(String::from).collect();
+    if let Some(split_index) = tokens.iter().position(|x| x == "--") {
+        Ok((tokens[..split_index].to_vec(), tokens[split_index + 1..].to_vec()))
+    } else {
+        Err(io::Error::new(io::ErrorKind::Other, "Header does not contain a -- separator"))
+    }
+}
+
+fn validate_secure_path(path: &Path) -> io::Result<()> {
+    if !path.exists() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "No such file or directory"));
+    }
+    let base_path = if path.is_relative() {
+        std::env::current_dir()?
+    } else {
+        PathBuf::new()
+    };
+    let mut current_path = PathBuf::new();
+    for component in base_path.components().chain(path.components()) {
+        current_path.push(component);
+        let stat = stat::lstat(&current_path)?;
+        if stat.st_uid != 0 {
+            return Err(io::Error::new(io::ErrorKind::Other, "File not hierarchically owned by root"));
+        }
+    }
+    if check_path_in_nosuid_mount(path)? {
+        return Err(io::Error::new(io::ErrorKind::Other, "File is in a nosuid mount"));
+    }
+    Ok(())
+}
+
+fn check_path_in_nosuid_mount(path: &Path) -> io::Result<bool> {
+    let path = path.canonicalize().unwrap();
+    let mounts = File::open("/proc/self/mounts")?;
+    let reader = io::BufReader::new(mounts);
+    if let Some(mount_options) = reader
+        .lines()
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .rev()
+        .filter_map(|line| {
+            let parts: Vec<_> = line.split_whitespace().collect();
+            if parts.len() >= 4 {
+                let (point, options) = (&parts[1], &parts[3]);
+                if path.starts_with(point) {
+                    return Some(options.to_string());
+                }
+            }
+            None
+        })
+        .next()
+    {
+        Ok(mount_options.split(',').any(|opt| opt == "nosuid"))
+    } else {
+        Err(io::Error::new(io::ErrorKind::Other, "File not in any mount"))
+    }
+}
